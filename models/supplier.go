b79b3ebc9db5d2012e653217ed8cbea6781ba5cd
Euller Pereira <euller88@gmail.com>
2015-12-02 14:32:51 -0300

    inital changes

--- models/supplier.go
+++ models/supplier.go
@@ -1,17 +1,24 @@
 package models
+
 // supplier
 
 import (
 	"github.com/gernest/utron"
-//	"time"
+	"github.com/jinzhu/gorm"
+	//	"time"
 )
 
 type Supplier struct {
-	Model	`schema:"-"`
-	Name		  string `schema:"name" sql:"type:varchar(100);not null"`
-	PhoneNumber string  `schema:"-"`
-	Note       string  `schema:"note" sql:"type:varchar(250);"`
-    Emails    []EmailSupplier `schema:"-"`
+	gorm.Model
+	Name         string          `schema:"name" sql:"type:varchar(100);not null"`
+	PhoneNumber  string          `schema:"-"`
+	Note         string          `schema:"note" sql:"type:varchar(250);"`
+	Emails       []EmailSupplier `schema:"-"`
+	EmailsLength int             `sql:"-"`
+}
+
+func (s *Supplier) SetEmailsLength() {
+	s.EmailsLength = len(s.Emails)
 }
 
 func init() {
